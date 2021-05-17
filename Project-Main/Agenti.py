befafbcf9a7113ae8d9ad1db32f78c0be2da72a4
mariomarktas <83081728+mariomarktas@users.noreply.github.com>
2021-05-17 12:28:46 +0200

    master

--- Project-Main/Agenti.py
+++ Project-Main/Agenti.py
@@ -1,6 +1,8 @@
 from cmath import exp
 import random
-from re import U
+
+from sqlalchemy import null
+import scipy.stats as stats
 import numpy as np
 import pandas as pd 
 import mesa
@@ -20,21 +22,35 @@ class technical(Agent):
 
     class_type = 'tech'
 
-    def __init__(self, status, money, assets, price.t):
+    def __init__(self, status, money, assets, price_t, k, unique_id):
         self.status = random.choice([-1,1]) #random initialization of opinion
         self.money = random.randrange(100,5000,1) #random initialization of avb money from 100 to 5000 and step 1
         self.assets = assets
+        self.k = k 
+        self.unique_id = unique_id
+
+    
+    def change_of_opinion(self):
+        if self.status == 1:
+            self.status = -1
+        else:
+            self.status = 1
 
     def does_its_thing(self):
         #calculate slope
         time_range = random.choice(range(50))
-        price_slope = pd.Series(np.gradient('price.series'[-time_range:-1]) #to be written later as price.slope attribute
+        price_slope = pd.Series(np.gradient('price.series'[-time_range:-1])) #to be written later as price.slope attribute
         alpha = random.random(0.5,1)
         U = alpha*(price_slope/k) #k è la frequenza con cui l'agente rivaluta la sua opinione, come la implementiamo?
-        shift_probability = k*np.exp(self.status*U) #normalizzazione??
-        np.random.normalvariate()
-        # per far funzionare il codice sotto bisogna controllare che shift_probability sia normalizzata tra 0 e 1 
+        shift_probability = stats.expon.cdf(self.status*U)*k #usando sta funzione la shift_probability è già normalizzata come una probabilità 
+
+        #DONE per far funzionare il codice sotto bisogna controllare che shift_probability sia normalizzata tra 0 e 1 
        
+        if random.random() < shift_probability: 
+            change_of_opinion(self)
+        # non so se è la sintassi giusta per scrivere sta roba
+
+
         # rettifico: non c'è alcun codice, come cazzo la implemento sta transizione di probabilità? 
         # bisogna normalizzare la shift_probability... com'cazz s normalizz sta probbbabbiliti wagliò
 
