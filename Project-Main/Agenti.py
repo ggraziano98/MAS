84bf3eee691ab46f634d960a6fea020b292034da
mariomarktas <83081728+mariomarktas@users.noreply.github.com>
2021-05-17 12:53:53 +0200

    master

--- Project-Main/Agenti.py
+++ Project-Main/Agenti.py
@@ -20,13 +20,13 @@ class technical(Agent):
     class_type = 'tech'
 
     def __init__(self, status, money, assets, price_t, k, unique_id):
+        super().__init__(status, money, assets, price_t, k, unique_id)
         self.status = random.choice([-1,1]) #random initialization of opinion
         self.money = random.randrange(100,5000,1) #random initialization of avb money from 100 to 5000 and step 1
         self.assets = assets
         self.k = k 
         self.unique_id = unique_id
 
-    
     def change_of_opinion(self):
         if self.status == 1:
             self.status = -1
@@ -39,26 +39,25 @@ class technical(Agent):
         price_slope = np.gradient('price.series'[-time_range:-1]) #to be written later as price.slope attribute
         alpha = random.randrange(5000,10000,1)*float(1e-4)
         U = alpha*(price_slope/k) #k è la frequenza con cui l'agente rivaluta la sua opinione, come la implementiamo?
-        shift_probability = stats.expon.cdf(self.status*U)*k #usando sta funzione la shift_probability è già normalizzata come una probabilità 
-
-        #DONE per far funzionare il codice sotto bisogna controllare che shift_probability sia normalizzata tra 0 e 1 
-       
+        shift_probability = stats.expon.cdf(self.status*U)*k #usando sta funzione la shift_probability è già normalizzata come una probabilità (a quanto pare)
         if random.random() < shift_probability: 
-            change_of_opinion(self)
-        # non so se è la sintassi giusta per scrivere sta roba
+            self.change_of_opinion()
+        # è la sintassi giusta per scrivere sta roba?
 
     def step(self):
         if self.status == 1:
-            #buy one thing
+            #self.buy_order(amount) - c'è ancora da definire il metodo buy
             pass
         else:
-            #sell one thing
+            #self.sell_order(amount) - c'è ancora da definire il metodo sell 
             pass
-        
-        while n = 0:
+
+        n = 0
+        while n != self.k:
             n += 1
-            if n == k: 
-                does_its_thing(self)
+            if n == self.k: 
+                self.does_its_thing()
+                # non so se è la sintassi giusta per scrivere sta roba
                 n = 0
           
 
