9727a2229a283b1296782b62a046989b2ecfb75f
Maria Elena Perruzzas <m.perruzza@yahoo.com>
2021-05-17 18:13:40 +0200

    master

--- Project-Main/Agenti.py
+++ Project-Main/Agenti.py
@@ -22,7 +22,7 @@ class technical(Agent):
     def __init__(self, status, money, assets, price_t, k, unique_id):
         super().__init__(status, money, assets, price_t, k, unique_id)
         self.status = random.choice([-1,1]) #random initialization of opinion
-        self.money = random.randrange(100,5000,1) #random initialization of avb money from 100 to 5000 and step 1
+        self.money = money #random initialization of avb money from 100 to 5000 and step 1
         self.assets = assets
         self.k = k 
         self.unique_id = unique_id
@@ -35,11 +35,14 @@ class technical(Agent):
 
     def does_its_thing(self):
         #calculate slope
-        time_range = random.choice(range(50)) # ogni agente calcola la slope col suo range temporale 
+
+        # ogni agente calcola la slope col suo range temporale 
+        time_range = random.randint(2, 50) 
         price_slope = np.gradient('price.series'[-time_range:-1]) #to be written later as price.slope attribute
-        alpha = random.randrange(5000,10000,1)*float(1e-4)
-        U = alpha*(price_slope/k) #k è la frequenza con cui l'agente rivaluta la sua opinione, come la implementiamo?
-        shift_probability = stats.expon.cdf(self.status*U)*k #usando sta funzione la shift_probability è già normalizzata come una probabilità (a quanto pare)
+        alpha = 0.5 + random.random() * 0.5
+        #k è la frequenza con cui l'agente rivaluta la sua opinione, come la implementiamo?
+        U = alpha*(price_slope/self.k) 
+        shift_probability = stats.expon.cdf(self.status*U) * self.k #usando sta funzione la shift_probability è già normalizzata come una probabilità (a quanto pare)
         if self.status == +1 and random.random() < shift_probability: 
             self.change_of_opinion()
         if self.status == -1 and random.random() > shift_probability:
@@ -89,21 +92,35 @@ class Noise(Agent):
         
         def does_its_thing(self):
                 
-                r = random.random()
+            r = random.random()
 
-                if r <= 0.5:
-                    if self.reddito >= prezzo_t:
-                        # buy_order(amount)
-                        pass
-                             
-                else:
-                    if self.n_azioni >= 0:    #ovviamente solo se ha azioni vende
-                        # sell_order(amount)
-                        pass
+            if r <= 0.5:
+                if self.reddito >= prezzo_t:
+                    # buy_order(amount)
+                    pass
+                            
+            else:
+                if self.n_azioni >= 0:    #ovviamente solo se ha azioni vende
+                    # sell_order(amount)
+                    pass
 
-        def step(self):
-            self.does_its_thing()    
+    def step(self):
+        self.does_its_thing()    
 
-           
+        
+class Trader(Agent):
+    def __init__(self, model, unique_id, money, assets, args):
+        super().__init__(self, model, unique_id)
+        self.money = money
+        self.assets = assets
 
+    def does_its_thing(self):
+        # Each class implements this
+        pass
+
+    def buy(self, n, price):
+        self.model.place_order('buy', n, price)
+
+    def sell(self, n, price):
+        self.model.place_order('sell', n, price)
 
